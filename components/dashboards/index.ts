539b6d57b4b742d94e71769b8770aeea4350eba9
shafayhere86-alt <shafayhere86@gmail.com>
2026-07-09 16:15:35 +0500

    Add dashboard components barrel export

---
+++ components/dashboards/index.ts
@@ -0,0 +1,46 @@
+// Admin Dashboard Components
+export { default as AdminDashboard } from './AdminDashboard'
+export { default as DashboardAnalytics } from './DashboardAnalytics'
+export { default as RevenueChart } from './RevenueChart'
+export { default as OrdersChart } from './OrdersChart'
+export { default as EmployeeManager } from './EmployeeManager'
+export { default as ClientManager } from './ClientManager'
+export { default as ProjectTracker } from './ProjectTracker'
+export { default as OrderManager } from './OrderManager'
+export { default as InvoiceManager } from './InvoiceManager'
+export { default as PaymentManager } from './PaymentManager'
+export { default as BlogManager } from './BlogManager'
+export { default as PortfolioManager } from './PortfolioManager'
+export { default as TestimonialManager } from './TestimonialManager'
+export { default as TicketManager } from './TicketManager'
+export { default as MessageCenter } from './MessageCenter'
+export { default as SettingsPanel } from './SettingsPanel'
+export { default as ActivityLog } from './ActivityLog'
+
+// Client Dashboard Components
+export { default as ClientDashboard } from './ClientDashboard'
+export { default as ClientOrders } from './ClientOrders'
+export { default as ClientInvoices } from './ClientInvoices'
+export { default as ClientProjects } from './ClientProjects'
+export { default as FileManager } from './FileManager'
+export { default as ClientMessages } from './ClientMessages'
+export { default as ClientTickets } from './ClientTickets'
+export { default as ClientProfile } from './ClientProfile'
+
+// Employee Dashboard Components
+export { default as EmployeeDashboard } from './EmployeeDashboard'
+export { default as AssignedProjects } from './AssignedProjects'
+export { default as TaskBoard } from './TaskBoard'
+export { default as WorkUpload } from './WorkUpload'
+export { default as EmployeeMessages } from './EmployeeMessages'
+export { default as PerformanceStats } from './PerformanceStats'
+
+// CRM Components
+export { default as CRMDashboard } from './CRMDashboard'
+export { default as LeadsManager } from './LeadsManager'
+export { default as DealsManager } from './DealsManager'
+export { default as FollowUpsManager } from './FollowUpsManager'
+
+// Order Management Components
+export { default as OrderCreation } from './OrderCreation'
+export { default as OrderTracking } from './OrderTracking'
